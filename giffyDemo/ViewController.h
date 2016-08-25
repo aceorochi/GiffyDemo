0852ce5afe04909feaf27ff9a1470e65d71c1f97
Pei Wu <aceorochi@gmail.com>
2016-08-25 14:16:06 -0700

    Initial Commit

---
+++ giffyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  giffyDemo
+//
+//  Created by Pei Wu on 8/25/16.
+//  Copyright © 2016 Pei Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
